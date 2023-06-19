93fd05c58442989c0beb1a6932e6f61973575d5f
arhippa11 <arhippa11@gmail.com>
2023-06-20 02:01:52 +0300

    Added prometheus support v1

--- globalconnector.py
+++ globalconnector.py
@@ -11,8 +11,19 @@ from dotenv import load_dotenv
 import timezonecheck
 import weatherapi
 import weatherstate
+import asyncio
+from prometheus_client import start_http_server, Summary, Gauge
+async def main():
+    start_http_server(8000)
 
+asyncio.run(main())
 load_dotenv()
+tempcgauge = Gauge('tempc', 'Temperature in celsius')
+tempfgauge = Gauge('tempf', 'Temperature in fahrenheit')
+feelslikecgauge = Gauge('feelslikec', 'Feels like temperature in celsius')
+windms = Gauge('windms', 'Wind speed in m/s')
+windmph = Gauge('windmph', 'Wind speed in mph')
+
 
 def globalconnector():
     print ("globalconnector.py has been loaded.")
@@ -36,12 +47,17 @@ def globalconnector():
     print("Checking the weather state...")
     weather_state = weatherstate.weatherstate(condition)
     print("Running the discord webhook...")
+    tempcgauge.set(temp_c)      # Increment by 1
+    tempfgauge.set(temp_f)      # Increment by 1
+    feelslikecgauge.set(feelslike_c)      # Increment by 1
+    windms.set(wind_m_s)      # Increment by 1
+    windmph.set(wind_mph)      # Increment by 1
     message = ("**Weather **" + weather_state +"\n" +"**State of the weather in "+ name +":** " +  condition + "\n" + "**Temperature:** " + str(temp_c) + "°C" + "\n" + "**Feels like:** " + str(feelslike_c) + "°C" + "\n" + "**Wind speed:** " + str(wind_m_s) + "m/s" + "\n"  + "**Region:** " + region + "\n" + "**Country:** " + country + "\n" + "**Last updated** ("+ timezone +")** :**_ "+ last_updated+  "_\n" + "**Local time** ("+ timezone +")** :**_ " + localtime + "_")
     webhook = DiscordWebhook(url=os.getenv("DISCORD_WEBHOOK_URL"), content=message)
     response = webhook.execute()
 
 while True:
     globalconnector()
-    time.sleep(300)
+    time.sleep(60)
     
 
