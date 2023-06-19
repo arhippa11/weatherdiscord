7e858c1aac4fc55d8bde11df8ec78c4c4b530f89
arhippa11 <arhippa11@gmail.com>
2023-06-19 13:09:30 +0300

    added basic prometheus testing

---
+++ prometheustest.py
@@ -0,0 +1,24 @@
+from prometheus_client import start_http_server, Summary
+import prometheus_client
+import random
+import time
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+from prometheus_client import Gauge
+g = Gauge('my_inprogress_requests', 'Description of gauge')
+g.inc()      # Increment by 1
+g.dec(10)    # Decrement by given value
+g.set(4.2)   # Set to a given value
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        process_request(1)
